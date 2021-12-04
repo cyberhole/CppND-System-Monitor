84df5bb44c448cfcce7d69e541c98850d72dbde0
cyberhole <ragomartins@gmail.com>
2021-12-04 19:44:40 +0000

    firs commit

---
+++ src/format.cpp
@@ -0,0 +1,39 @@
+#include <string>
+
+#include "format.h"
+
+using std::string;
+
+
+
+// TODO: Complete this helper function
+// INPUT: Long int measuring seconds
+// OUTPUT: HH:MM:SS
+// REMOVE: [[maybe_unused]] once you define the function
+
+string Format::ElapsedTime(long seconds) { 
+    /*Accepts long as input returns a string;
+      Converts seconds into hours:minutes:seconds
+    */
+
+   //Conversion of the seconds into the different "fields"
+int hours_=seconds/3600;
+int minutes_=(seconds/60-hours_*60);
+int seconds_=(seconds-minutes_*60-hours_*3600);
+    //Creation of the string to output the time
+std::string time=convertTime(hours_)+":"+convertTime(minutes_)+":"+convertTime(seconds_);
+return time;}
+
+
+string Format::convertTime(int number){
+    /*Function accepts an int and returns a string.
+    Verifies if the int is bigger than 10. If it isn't it adds a 0 on the left. 
+    To format the output of the time on the elapsed time function.
+    */
+std::string number_; 
+if (number>=10)
+number_=std::to_string(number);
+else 
+number_="0"+std::to_string(number);
+return number_;
+}
